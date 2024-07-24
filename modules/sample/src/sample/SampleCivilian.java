b65bf028cf54c12a35cbcc74358e9f088a9ced4a
Leongw994 <leongw99@gmail.com>
2024-07-24 15:21:54 +0100

    changes made 24/07/2024

--- modules/sample/src/sample/SampleCivilian.java
+++ modules/sample/src/sample/SampleCivilian.java
@@ -112,14 +112,14 @@ public class SampleCivilian extends AbstractSampleAgent<Civilian> {
       say(OUCH, time);
     }
     if (buriedness > 0 && random.nextDouble() < helpProbability) {
-      say(HELP, time);
+//      say(HELP, time);
     }
 
-//    LOG.info("Calling for help");
-//    int x = me().getX();
-//    int y = me().getY();
-//    String message = String.format("Help %d %d", x, y);
-//    sendSpeak(time, 1, message.getBytes());
+    LOG.info("Calling for help");
+    int x = me().getX();
+    int y = me().getY();
+    String message = String.format("Help %d %d", x, y);
+    sendSpeak(time, 1, message.getBytes());
 
     if (damage == 0 && buriedness == 0) {
       // Run for the refuge
